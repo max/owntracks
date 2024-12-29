26add10179be6e7840d8cde265d519f3d76c19a4
Max Schoening <max@max.dev>
2024-12-29 12:30:42 -0800

    First stab at configuring docker-compose
    This commit set sup docker-compose for the broker and recorder

--- broker/entrypoint.sh
+++ broker/entrypoint.sh
@@ -1,24 +1,22 @@
 #!/bin/sh
 
 if [ -z "$MQTT_USERNAME" ] || [ -z "$MQTT_PASSWORD" ]; then
-    echo "Error: MQTT_USERNAME and MQTT_PASSWORD must be set"
+    echo "Error: MQTT_USERNAME and MQTT_PASSWORD must be set."
     exit 1
 fi
 
-if [ -z "$TAILSCALE_AUTHKEY" ]; then
-    echo "Error: TAILSCALE_AUTHKEY must be set"
-    exit 1
+if [ -n "$TAILSCALE_AUTHKEY" ]; then
+    echo "TAILSCALE_AUTHKEY is set. Setting up Tailscale."
+
+    /usr/local/bin/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
+    /usr/local/bin/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=mqtt
+else
+    echo "Warning: TAILSCALE_AUTHKEY not set. Skipping Tailscale."
 fi
 
-echo "Creating mosquitto password for $MQTT_USERNAME"
+echo "Creating mosquitto password for $MQTT_USERNAME."
 mosquitto_passwd -c -b /mosquitto/config/passwd "$MQTT_USERNAME" "$MQTT_PASSWORD"
 
-# Ensure permissions are correct
 chown -R mosquitto:mosquitto /mosquitto
 
-# Run Tailscale
-/usr/local/bin/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/usr/local/bin/tailscale up --auth-key=${TAILSCALE_AUTHKEY} --hostname=mqtt
-
-# Run Mosquitto in the foreground
 exec mosquitto -c /mosquitto/config/mosquitto.conf
